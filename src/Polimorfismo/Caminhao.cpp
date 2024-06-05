9016397ecdfdee2870ee474dc68e6f29c09fbc05
Amanda Fiaux <65243181+Fiaux12@users.noreply.github.com>
2024-06-05 16:58:31 -0300

    :construction: feat: create veiculos

---
+++ src/Polimorfismo/Caminhao.cpp
@@ -0,0 +1,27 @@
+#ifndef CAMINHAO
+#define CAMINHAO
+#include <string>
+#include <iostream>
+#include "Veiculo.cpp"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Caminhao : public Veiculo {
+    private:
+        int eixos;
+
+    public:
+        Caminhao(const string& marca, const string& modelo, int capacidade, int eixos)
+            : Veiculo(marca, modelo, capacidade), eixos(eixos)
+        {}
+        
+        void exibirDados() const override 
+        {
+            cout << "Caminhão - ";
+            Veiculo::exibirDados();
+            cout << "Eixos: " << eixos << "\n";
+        }
+};
+#endif
---
+++ src/Polimorfismo/Veiculo.cpp
@@ -0,0 +1,27 @@
+#ifndef VEICULO
+#define VEICULO
+#include <string>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+class Veiculo {
+    protected:
+        string marca;
+        string modelo;
+        int capacidade; // Capacidade de carga em toneladas
+
+    public:
+        Veiculo(const string& marca, const string& modelo, int capacidade)
+            : marca(marca), modelo(modelo), capacidade(capacidade) 
+        {}
+
+        virtual void exibirDados() const 
+        {
+            cout << "Marca: " << marca << ", Modelo: " << modelo <<
+            ", Capacidade: " << capacidade << " toneladas\n";
+        }
+};
+#endif
